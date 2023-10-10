4a602a87fffc6700ff709b2c4f4773412b695272
Ryan <131712164+ryanNS3@users.noreply.github.com>
2023-10-10 17:25:56 -0300

    Add files via upload

--- src/components/section-personalidades/Personalidades.jsx
+++ src/components/section-personalidades/Personalidades.jsx
@@ -1,59 +1,48 @@
 import React from "react";
 import "../section-personalidades/style.css"
 import fotoVanGogh from "../../assets/vangogh.png"
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-// import 'swiper/css/navigation';
-
-// import {  Navigation } from "swiper/modules";
-
-
+import {motion}  from 'framer-motion'
 
 function Personalidades(){
 
     return(
-
         <section className="box-personalidades">
+
             <article className="titulo-perso">
                  <h2>Personalidades</h2>
             </article>
 
-            <Swiper
-            slidesPerView={4}
-            spaceBetween={30}
-            centeredSlides={false}
+            <motion.article className="carroussel">
+                <motion.article className="innerCarroussel">
 
-            //  pagination={{
-            //   clickable: true,
-            //  }}
-            modules={[]}
-            className="mySwiper"
-            >
+                    <motion.section className="item">
+                        <img  src={fotoVanGogh}/>
+                    </motion.section>
+
+                    <motion.section className="item">
+                        <img  src={fotoVanGogh}/>
+                    </motion.section>
+
+                    <motion.section className="item">
+                        <img  src={fotoVanGogh}/>
+                    </motion.section>
+
+                    <motion.section className="item">
+                        <img  src={fotoVanGogh}/>
+                    </motion.section>
+                    
+                    <motion.section className="item">
+                        <img  src={fotoVanGogh}/>
+                    </motion.section>
+
+
+                </motion.article>
+            </motion.article>
+
+
                 
-                <SwiperSlide>
-                <article className="artistas">
-                    <div className="box-img-perso">
-                        <img src={fotoVanGogh} alt="fotovangogh" />
-                        <span className="decorador-perso"></span>
-                        <p>Van gogh</p>
-                    </div>
-
-                </article>
-
-                </SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
-
-            </Swiper>
-    </section>
+        </section>
+      
     )
 }
 
