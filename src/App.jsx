3cde0a51921530fd8a84d5617a287728b38dfc1d
Ryan <131712164+ryanNS3@users.noreply.github.com>
2023-10-13 11:07:51 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react"
-import "./app.css"
 import NavBar from "./components/navBar/navBar"
 import Header from "./components/header/Header"
 import { MovimentosProvider } from "./context/globalMovi"
 import { ObrasProvider } from "./context/globalObras"
 import { PersonProvider } from "./context/globalPerson"
 import Personalidades from "./components/section-personalidades/Personalidades"
+import "./app.css"
 
 
 
