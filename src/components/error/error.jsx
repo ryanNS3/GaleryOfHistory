b575ae05b7c7fc610881fe8d51b4d17a55306be9
Ryan <131712164+ryanNS3@users.noreply.github.com>
2023-10-19 09:00:00 -0300

    Add files via upload

--- src/components/error/error.jsx
+++ src/components/error/error.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ilustracaoErro from "../../assets/erroImg.svg"
+import "./styleError.css"
 
 const Erro = () =>{
     return(
-        <section className="box-error" aria-labelledby="titulo-erro">
+        <section className="animacaoEsquerda box-error" aria-labelledby="titulo-erro">
             <h1 id="titulo-erro">Ocorreu algum erro com os nossos serviços, retornaremos em breve</h1>
-            <img src={ilustracaoErro} alt="ilsutração de erro "/>
-
+            <img src={ilustracaoErro} alt="ilsutração de erro " className="img-erro"/>
         </section>
     )
 }
